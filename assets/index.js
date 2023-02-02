13b398487d2acd1acdef35a61688c2aa0d45876c
Branidni <98710658+Branidni@users.noreply.github.com>
2023-02-01 18:46:33 -0800

    add html structure, setup clean workspace 4 css/js

---
+++ assets/index.js
@@ -0,0 +1,14 @@
+// '//' standard foward slash comments are just my todo list/notes to myself
+// a '//\\-COMMENT-//\\' style comment describes the purpose of my functions
+var dateQueryFrom = document.getElementById('#date-start'); // have the user input search the api for the specified date of items coming in
+var dateQueryTo = document.getElementById('#date-end'); // both these variables will be use to query the api for product info in a given date range
+var products = document.getElementById('#products-list'); // both product & quantity var's should append synomymously on the same row
+var quantity = document.getElementById('#products-quantity'); // list should line up with the products variable on the same row
+
+// first get all the interactive items on the page working  
+
+// next you will need to get access to the apiData
+
+// then you will destructure what you want from it
+
+// append the required data to the page
