2721e17ab08a3dcdac02f3b747f44d02054594af
Branidni <98710658+Branidni@users.noreply.github.com>
2023-02-02 18:04:59 -0800

    change line 5 & 6 to doc.querySel not getbyid

--- assets/index.js
+++ assets/index.js
@@ -2,8 +2,8 @@
 // a '//\\-COMMENT-//\\' style comment describes the purpose of my functions
 // var dateQueryFrom = document.getElementById('#date-start'); // have the user input search the api for the specified date of items coming in
 // var dateQueryTo = document.getElementById('#date-end'); // both these variables will be use to query the api for product info in a given date range
-var products = document.getElementById('#products-list'); // both product & quantity var's should append synomymously on the same row
-var quantity = document.getElementById('#products-quantity'); // list should line up with the products variable on the same row
+var products = document.querySelector('#products-list'); // both product & quantity var's should append synomymously on the same row
+var quantity = document.querySelector('#products-quantity'); // list should line up with the products variable on the same row
 
 // the date input variables don't store my unput values but its not too important right now so i'll worry about it later
 // first get all the interactive items on the page working  
