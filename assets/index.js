c70247efc0dba85c9c0e4804ae400821d787115b
Branidni <98710658+Branidni@users.noreply.github.com>
2023-02-02 22:59:04 -0800

    logic works for  test data & fixd bugs in html/css

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,7 @@
 // '//' standard foward slash comments are just my todo list/notes to myself
 // a '//\\-COMMENT-//\\' style comment describes the purpose of my functions
-// var dateQueryFrom = document.getElementById('#date-start'); // have the user input search the api for the specified date of items coming in
-// var dateQueryTo = document.getElementById('#date-end'); // both these variables will be use to query the api for product info in a given date range
-var products = document.querySelector('#products-list'); // both product & quantity var's should append synomymously on the same row
-var quantity = document.querySelector('#products-quantity'); // list should line up with the products variable on the same row
+var products = document.querySelector('#products'); // both product & quantity var's should append synomymously on the same row
+var quantities = document.querySelector('#quantities'); // list should line up with the products variable on the same row
 
 // the date input variables don't store my unput values but its not too important right now so i'll worry about it later
 // first get all the interactive items on the page working  
@@ -15,7 +13,7 @@ function ProductData(product, quantity) {  // object constructor for the api dat
     this.quantity = quantity;
   }
   //\\-FETCH FUNCTION TO GRAB DATA-//\\
-  fetch('https://jsonplaceholder.typicode.com/users')
+  fetch('https://jsonplaceholder.typicode.com/users')  // that url is for a json testing page its not useful data at all except for testing
      .then(response => response.json()) // converts response to a json object
      //\\-CREATES A USER OBJECT AND STORES VALUES WITHIN AN ARRAY-//\\
      .then(users => {  
@@ -25,9 +23,23 @@ function ProductData(product, quantity) {  // object constructor for the api dat
               let quantity = user.id;  // the quantity is the really data we want. user id just so happens to be an integer as well so we used it for testing
               let productData = new ProductData(product, quantity);  // the var 'productData' now has the desired dummy data within  
               data.push(productData);  // pushes the dummy data into the empty array we made to store the newly constructed objects each with a username and id number 
-          });
+          })
           console.log(data);
           return data;
+     })
+     //\\-(DUMMY DATA) IS APPENDED TO THE PAGE-//\\
+     .then(data => {
+        data.forEach(({product, quantity}) => {  // for loop 
+            let productList = document.createElement('li');  // creates 'li' element
+            productList.setAttribute("id", "product-item"); // sets attributes for styling
+            productList.innerHTML = `${product}`;  // assigns the inner html with the object literal `${product}`
+            products.appendChild(productList);  // appends the newly made element to the page 
+
+            let productQuantity = document.createElement('li');
+            productQuantity.setAttribute("id", "quantity-list");
+            productQuantity.innerHTML = `${quantity}`;
+            quantities.appendChild(productQuantity);
+        });
      });
 
 // append the required data to the page
