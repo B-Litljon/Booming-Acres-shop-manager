7eea92f4a7aabff1efdc833928e7a8d95c9076d9
Branidni <98710658+Branidni@users.noreply.github.com>
2023-02-13 18:47:30 -0800

    setup routes

--- routes/oAuthRoutes.js
+++ routes/oAuthRoutes.js
@@ -1,7 +1,48 @@
+//\\-VARIABLES & IMPORTS-//\\
 const express = require('express');
-const router = express.router()
+const codeGen = require('./../helpers/codeGen');
+const authRouter = express.Router();
+require('dotenv').config();
+require('./../helpers/codeGen')
+const apiKey = process.env.API_KEY;
+const redirectUri = ('https://b-litljon.github.io/booming-acres-shop-manager');
+const scope = ('transactions_r');
+const state = codeGen.stateGenerator;
+const verifier = codeGen.codeVerifierGen;
+console.log(apiKey);
+console.log(redirectUri);
+console.log(scope);
+console.log(state);
+console.log(verifier);
+
+
+//\\-ROUTES-//\\
+authRouter.get('/authorize', (req, res) => {
+    const etsyUrl = `https://www.etsy.com/oauth/connect?
+    response_type=code
+    &client_id=${apiKey}
+    &redirect_uri=${redirectUri}
+    &scope=${scope}
+    &state=${state}
+    &code_challenge=${verifier}
+    &code_challenge_method=S256`
+  
+    res.redirect(etsyUrl);
+  });
+authRouter.get('/authorize', (req, res) => {
+    res.render('authentication', {
+        oAuthRoute: oAuthRoutes
+    })
+})
 
 // this route will handle all the oath credentials
 
-
-module.exports = router;
+//\\-MODULE EXPORTS-//\\
+module.exports = {
+    apiKey,
+    redirectUri,
+    scope,
+    state,
+    verifier,
+    authRouter
+  };  
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 
 const app = express();
-PORT: 3001
+
+const oAuthRoutes = require('./routes/oAuthRoutes')
+
+//new line of code
+app.set('view engine', 'hbs');
+
+app.use('/oAuth', oAuthRoutes);
+app.get('/authorize', (req, res) => {
+    res.send('authorize endpoint');
+});
+
+
+
+const PORT = 3001
 app.listen(PORT, () =>
 console.log(`listening on port: ${PORT}`)
 )
