20d487565be75bf0b50ff6ff9b305aed36de5728
Branidni <98710658+Branidni@users.noreply.github.com>
2023-02-15 16:21:28 -0800

    remove redundant code

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const hbs = require('hbs')
 const oAuthRoutes = require('./routes/oAuthRoutes')
 
 //\\-HANDLEBARS-//\\
-app.set('views', path.join(__dirname, 'views')); 
-app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'views')); 
+// app.set('view engine', 'hbs');
+
 
 //\\-AUTHENTICATION ROUTES-//\\
 app.use('/oAuthRoutes', oAuthRoutes.authRouter);
@@ -14,10 +15,10 @@ app.get('/authorize', (req, res) => {
   res.send('authorize endpoint');
  });
 
-//\\-PATH TO STATIC PAGES-//\\
+// needs to be removed and updated to display handlebars instead 19-21
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/pages/index.html'))
+  res.sendFile(path.join(__dirname, 'public/pages/index.html'));
 });
 
 const PORT = 3001
