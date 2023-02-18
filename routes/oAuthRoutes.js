38ec7bd275e9f04bf3ffd0160bb3c1407b63d6f5
Branidni <98710658+Branidni@users.noreply.github.com>
2023-02-17 21:19:18 -0800

    added encryption to token, rdy 4 storage

--- routes/oAuthRoutes.js
+++ routes/oAuthRoutes.js
@@ -35,6 +35,8 @@ authRouter.get('/authorize', (req, res) => {
 //\\-AUTH TOKEN ENCRYPTION AND STORAGE-//\\
 
 // this route will handle extracting the oAth token from the callback url, =>
+const bcrypt = require('bcrypt')
+const saltRounds = 12;
 authRouter.get('/callback', async (req, res) => {
   const { code } = req.query; // extract the OAuth code from the query parameters in the callback URL
   // use the OAuth code to obtain an access token
@@ -43,12 +45,17 @@ authRouter.get('/callback', async (req, res) => {
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ code }),
+    body: JSON.stringify({ code }), // code is the token
   });
-  const result = await response.json();
+  const result = await response.json(); // result is the code
   // handle the response and redirect to the appropriate page
 
   // encrypt and store the access token securely, such as in a database or environment variable
+  // Hash and store the access token securely
+  const accessToken = result.access_token; 
+  const hashedToken = await bcrypt.hash(accessToken, saltRounds); // hashed token now safe for storage
+  // Store the hashed token in a database or environment variable
+  
   res.redirect('/'); // redirect the user to a success page or another appropriate page
 });
 
